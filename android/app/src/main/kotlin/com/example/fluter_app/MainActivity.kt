aad6dbe3a8474a5580b13b7dfb01cbd8808b3b38
Shanthalingam Wishvagaran <shanthalingam_2016126@fot.ruh.ac.lk>
2022-07-01 16:25:47 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fluter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/InputWrapper.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class InputWrapper extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    ret
+  }
+}
